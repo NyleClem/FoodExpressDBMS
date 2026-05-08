4d4cbc483bfeacbfd92342a5dad53498195fc0fe
Fatika-02 <fatika2@students.towson.edu>
2026-05-08 11:30:32 -0400

    update

---
+++ FoodExpressUI.java
@@ -0,0 +1,23 @@
+import java.awt.BorderLayout;     // Swing GUI classes (JFrame, JButton, JTextField, etc.)
+import java.awt.GridLayout;        // Layout managers like BorderLayout, GridLayout
+import java.sql.Connection;        // JDBC classes for database connection
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class FoodExpressUI extends JFrame{
+
+    public static void main (String[] arg)
+    {
+
+    }
+}
