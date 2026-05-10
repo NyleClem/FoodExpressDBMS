b42a38c43c2b857075190b8f821be9eb8ca85e09
Fatika-02 <fatika2@students.towson.edu>
2026-05-10 17:31:47 -0400

    Updataed the sql operation (#28)

--- sql/sql_operations.sql
+++ sql/sql_operations.sql
@@ -1,20 +1,25 @@
 USE FoodExpress2;
 
-UPDATE Vendor
-SET Location = '25th Avenue', VendorName= 'Dominos'
-WHERE VendorID = 1;
-UPDATE Vendor
-SET Location = '448 Baker Street', VendorName= 'Five Guys'
-WHERE VendorID = 2;
-UPDATE Vendor
-SET Location = '345 Jackson Blvd', VendorName= 'Pandora'
-WHERE VendorID = 3;
-UPDATE Vendor
-SET Location = '5th Maple Street', VendorName= 'IHOP'
-WHERE VendorID = 4;
-UPDATE Vendor
-SET Location = '68th Oak Street', VendorName= 'Dennys'
-WHERE VendorID = 5;
+UPDATE Customer
+SET Address  = '45 Apple Lane', Phone= '(443) 999-0102'
+WHERE CustomerID = 1;
+UPDATE Customer
+SET Address  = '112 Holly court', Phone= '(443) 890-2229'
+WHERE CustomerID = 2;
+UPDATE Customer
+SET Address  = '34th street', Phone= '(443) 390-0056'
+WHERE CustomerID = 3;
+
+UPDATE Driver
+SET DriverName = 'Charlie Brown', Phone ='(443) 987-001'
+WHERE DriverID =100;
+UPDATE Driver
+SET DriverName = 'Lily Rose', Phone ='(443) 334-2111'
+WHERE DriverID =200;
+UPDATE Driver
+SET DriverName = 'Stacy Fakename', Phone = '(443) 555-5555'
+WHERE DriverID =300;
+
 
 Select V.Location, V.VendorName,M.ItemName,M.Price FROM Vendor V
 LEFT JOIN MenuItem M ON  V.VendorID = M.VendorID
@@ -29,6 +34,9 @@ Select V.VendorName, COUNT(M.VendorID) AS 'Numbers of items'
 FROM Vendor V
 LEFT JOIN MenuItem M ON M.VendorID = V.VendorID
 GROUP BY V.VendorName;
+
+
+
 
 
 
